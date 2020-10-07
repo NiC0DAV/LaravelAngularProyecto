eaf2e525701fa3e0cd5aaa875004967f9612c602
NicLux404 <52676193+NiC0DAV@users.noreply.github.com>
2020-10-07 17:45:48 -0500

    Se finaliza la creacion de metodo para crear y almacenar categorias

--- UdemyProject/app/Http/Controllers/categoryController.php
+++ UdemyProject/app/Http/Controllers/categoryController.php
@@ -8,6 +8,9 @@ use App\Models\Category;
 
 class categoryController extends Controller
 {
+    public function __construct(){
+        $this->middleware('api.auth', ['except' => ['index', 'show']]); 
+    }
     public function index(){
         $categories = Category::all();
 
@@ -36,5 +39,48 @@ class categoryController extends Controller
         }
 
         return response()->json($data, $data['code']);
+    }
+
+    public function store(Request $request){
+        //Recoger los datos por post
+        $json = $request->input('json', null);
+        $params_array = json_decode($json, true);
+
+        if(!empty($params_array)){
+            //Validar los datos
+            $validate = \Validator::make($params_array, [
+                'name' => ['required']
+            ]);
+            //Guardar la categoria
+            if($validate->fails()){
+                $data = [
+                    'code' => 400,
+                    'status' => 'Error',
+                    'message' => 'No se ha guardado la categoria'
+                ];
+            }else{
+                $category = new Category();
+                $category->name = $params_array['name'];
+                $category->save();
+                
+                $data = [
+                    'code' => 200,
+                    'status' => 'success',
+                    'category' => $category
+                ];
+
+            }
+        }else{
+            $data = [
+                'code' => 400,
+                'status' => 'Error',
+                'message' => 'No has enviado ninguna categoria'
+            ];
+        }   
+
+
+        //Devolver el resultado
+        return response()->json($data, $data['code']);
+
     }
 }
