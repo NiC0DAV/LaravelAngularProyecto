d075ee1195700b23e53a097c5e706d0c2e53f9f0
NicLux404 <52676193+NiC0DAV@users.noreply.github.com>
2020-10-27 16:06:57 -0500

    Se termina la configuracion de rutas

--- blog-angular/src/app/app.routing.ts
+++ blog-angular/src/app/app.routing.ts
@@ -5,13 +5,16 @@ import { Routes, RouterModule } from '@angular/router';
 //Importar Componentes
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent} from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 
 //Creacion de rutas
 const appRoutes: Routes = [
-    {path: '', component: LoginComponent},
-    {path: 'inicio', component:LoginComponent},
+    {path: '', component: HomeComponent},
+    {path: 'inicio', component:HomeComponent},
     {path: 'login', component:LoginComponent},
-    {path: 'register', component:RegisterComponent}
+    {path: 'register', component:RegisterComponent},
+    {path: '**', component:ErrorComponent}
 ];
 
 //Exportar configuracion
