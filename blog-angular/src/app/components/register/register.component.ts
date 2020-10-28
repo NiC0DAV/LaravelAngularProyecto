1116c51501dfd59da7efad16d88453e303afcfbb
NicLux404 <52676193+NiC0DAV@users.noreply.github.com>
2020-10-27 23:32:19 -0500

    Se mejora la validacion de todos los campos de registro, ademas se crea el servicio de registro para iniciar la inyeccion ajax de datos a la BD por medio del API

--- blog-angular/src/app/components/register/register.component.ts
+++ blog-angular/src/app/components/register/register.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
   public page_title: string;
+  public user: User;
 
-  constructor() {
+  constructor(
+    private _userService: UserService
+  ) {
     this.page_title = 'Registrate';
+    this.user = new User(1, '', '', 'ROLE_USER', '', '' , '', '');  
    }
 
   ngOnInit(): void {
     console.log("Componente de registro lanzado!!");
   }
 
+  onSubmit(form){
+    console.log(this._userService.test());
+    console.log(this.user);
+    form.reset();
+  }
 }
